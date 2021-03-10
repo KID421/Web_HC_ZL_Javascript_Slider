5edd8353b8f0df3c7a92d61bfe72f8cc1825850d
KID421 <suchild7@gmail.com>
2021-03-10 19:02:51 +0800

    上一張功能完成

--- main.js
+++ main.js
@@ -2,26 +2,30 @@
 var index = 0;
 // 下一張按鈕
 var nextBtn = document.querySelector("#slider-next");
+var prevBtn = document.querySelector("#slider-prev");
 // 取得所有的輪播項目
 var items = document.getElementsByClassName("slider-item");
 // 輸出輪播項目的數量
-console.log("輪播項目的數量：" + items.length);
+// console.log("輪播項目的數量：" + items.length);
 
 // 下一張按鈕的功能
 function next() {
     index++;
     // 如果編號是 項目的數量 就將編號改為 零
-    if (index == items.length) {
-        index = 0;
-    }
-    
-    console.log("編號：" + index);
+    if (index == items.length) index = 0;
+    showItem();
+}
 
+function prev() {
+    index--;
+
+    if (index == -1) index = items.length - 1;
     showItem();
 }
 
 //下一張按鈕.點擊 = 下一張按鈕的功能
 nextBtn.onclick = next;
+prevBtn.onclick = prev;
 
 // 顯示項目
 function showItem() {
